b4c2cd895ebc646e3a7ba2f98e3624a547fe6893
Thuo <thuohm@gmail.com>
2016-02-04 17:27:58 +0300

    Validate asset code for uniqueness

--- app/assets/forms.py
+++ app/assets/forms.py
@@ -1,6 +1,7 @@
 from wtforms import validators, StringField, SubmitField, TextAreaField, \
     DateField, RadioField
 from flask_wtf import Form
+import app
 
 
 class AddAssetForm(Form):
@@ -14,6 +15,14 @@ class AddAssetForm(Form):
     purchased = DateField("Date Purchased",
                           format='%d/%m/%Y', validators=[validators.Optional()])
     submit = SubmitField("Add Asset")
+
+    def validate(self, **kwargs):
+        valid = Form.validate(self)
+        if app.models.Asset.query.filter_by(code=self.code.data).first() is not None:
+            self.code.errors.append("Andela Serial Code must be unique")
+            valid = False
+
+        return valid
 
 
 class AssignAssetForm(Form):
